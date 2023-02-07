271cfcc5f2bc8310b4def57b838cfd0fc05bd96e
Abishek yadav <abhiyadav172408@gmail.com>
2023-02-07 22:44:04 +0530

    circumference

---
+++ Circumference.c
@@ -0,0 +1,11 @@
+//Calculate circumference ofa circle
+#include <stdio.h>
+int main()
+{
+	int r=17;
+	float pi=3.14;
+	printf("Enter the value of radius \n");
+	scanf("%d",&r);
+	printf("The circumference of circle is %f",2*r*pi);
+	return 0;
+}
