6ce98227eed8f166522bd625fd5249a8504a4cbe
Abishek yadav <abhiyadav172408@gmail.com>
2023-03-23 13:40:13 +0530

    Array1

---
+++ Array1.c
@@ -0,0 +1,14 @@
+//Array - it is an derived data type
+
+//1 d aarray and 2 d
+
+
+
+#include<stdio.h>
+int main(){
+	int a[5]={1,2,3,4,5};
+	int i;
+	for(i=0;i<5;i++){
+		printf("%d\n",a[i]);
+	}
+}
